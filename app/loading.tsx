35a443b370471dd0bafaca23a700babea406a380
Charles042 <ekechukwuma20@gmail.com>
2025-07-04 00:49:38 +0100

    -feat: added a loader state

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex w-full justify-center items-center h-screen">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
